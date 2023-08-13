64337e628c9ae61cfa76a84648ae9569df942939
Dmitri Shavshin <shavshindm@outlook.com>
2023-08-13 21:45:29 +0300

    reading from text file and display in console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string filePath = "Passwords.txt";
+            try
+            {
+                string[] rows = File.ReadAllText(filePath).Split(new[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string row in rows)
+                {
+                    Console.WriteLine(row);
+                    Console.WriteLine("-------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
